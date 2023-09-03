5b41f754091facd979c761ada76579ed993c53c8
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-09-03 09:58:50 +0000

    Test2

--- steps.sh
+++ steps.sh
@@ -12,3 +12,14 @@ source venv/bin/activate
 
 # Install Requirements
 pip install -r requirements.txt
+
+# CodeBuild Role
+Code Build Role - codebuild-AWS-DevSecOps-Project-service-role
+
+# Change Flask version and check
+Flask==2.2.5
+Flask==2.0.2
+
+# Used following git command to mirro github to codecommit
+git clone --mirror https://github.com/sanju2/AWS-DevSecOps-Project.git AWS-DevSecOps-Project
+git push https://git-codecommit.us-east-2.amazonaws.com/v1/repos/AWS-DevSecOps-Project
